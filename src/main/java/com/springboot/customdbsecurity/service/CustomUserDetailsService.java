dae8f3ae8e6c32c433cc42d84ae7be66870a222a
Dharani V <vaddepallidharani39@gmail.com>
2024-01-10 23:57:56 -0500

    Springboot Custom DB security changes Signed-off-by: Dharani V <vaddepallidharani39@gmail.com>

---
+++ src/main/java/com/springboot/customdbsecurity/service/CustomUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.springboot.customdbsecurity.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.springboot.customdbsecurity.Repository.UsersRepository;
+import com.springboot.customdbsecurity.model.CustomUserDetails;
+import com.springboot.customdbsecurity.model.Users;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Autowired
+    private UsersRepository usersRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    	Optional<Users> dbUserData = usersRepository.findByName(username);
+    	//select * from users join roles where user.name=username;
+    	//Optional<Users> ==> our database data
+    	//Need to map database data to UserDetails object  //4 approaches
+
+    	dbUserData
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        //Map is going to Cloning
+        //CustomUserDetails -> Our object and UserDetails -> Spring security Object
+        // CustomerUserDetails springData =new CustomerDetails();
+       // springData.setUserName("UserObject of Username");
+        //springData.setPassword("UserObject of password");
+        //springData.isActive("true")
+        //
+
+
+       return dbUserData
+               .map(CustomUserDetails::new).get();
+
+
+    }
+}
